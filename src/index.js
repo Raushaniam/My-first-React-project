1877fb6209ac90e764eef446aea9fc1c4ab4e45e
Raushaniia <mullakhmetova18@gmail.com>
2023-05-15 18:05:45 +0400

    feat: add context to webpack.config.js and install css-loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import Post from "./Post";
+import Post from './Post';
+import json from './assets/json.json';
+import './styles/style.css';
 
 const post = new Post('Webpack Post Title');
 
 console.log('Post to string:', post.toString());
+
+console.log('JSON:', json);
