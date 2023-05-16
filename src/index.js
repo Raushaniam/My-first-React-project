7012641f2f7d485b24e1ba5c38fe7a5a16017e86
Raushaniia <mullakhmetova18@gmail.com>
2023-05-16 18:02:28 +0400

    feat: add parameter resolve to webpack.config and install jquery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import Post from './Post';
+import * as $ from 'jquery';
+import Post from '@models/Post';
 // import json from './assets/json.json';
 // import xml from './assets/data.xml';
 // import csv from './assets/data.csv';
@@ -7,7 +8,7 @@ import './styles/style.css';
 
 const post = new Post('Webpack Post Title', WebpackLogo);
 
-console.log('Post to string:', post.toString());
+$('pre').html(post.toString());
 
 // console.log('JSON:', json);
 // console.log('XML:', xml);
