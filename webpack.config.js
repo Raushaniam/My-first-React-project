289c190bcaef6532a71e51b433279f3a42a41e0b
Raushaniia <mullakhmetova18@gmail.com>
2023-05-17 18:23:18 +0400

    feat: add favicon and mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// const isDev = process.env.NODE_ENV === 'development';
+// console.log('IS DEV:', isDev);
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -38,13 +43,34 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin( {
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ],
+            options: {
+                concurrency: 100,
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    "css-loader",
+                ]
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
