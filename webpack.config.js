704ba61a6bf09ba293080a6e56a3647fb7fc765b
Raushaniia <mullakhmetova18@gmail.com>
2023-05-19 15:59:37 +0400

    feat: add babel

--- webpack.config.js
+++ webpack.config.js
@@ -116,6 +116,26 @@ module.exports = {
             {
                 test: /\.csv$/,
                 use: ['csv-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
